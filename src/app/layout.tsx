0aa9d1e0ff66e0c01736290971c10af53fc1a52c
Vũ Vương An <hackeranvip@gmail.com>
2025-07-04 17:05:42 +0700

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,22 @@ const nunitoSans = Nunito_Sans({
 export const metadata: Metadata = {
   title: "An Vu Vuong | Frontend Developer",
   description: "An Vu Vuong | Frontend Developer",
+  openGraph: {
+    title: "An Vu Vuong | Frontend Developer",
+    description: "Portfolio of An Vu Vuong, a passionate Frontend Developer. Explore projects, skills, and contact information.",
+    url: "https://um1chc5.com/",
+    siteName: "An Vu Vuong Portfolio",
+    images: [
+      {
+        url: "/public/file.svg", // Update with your actual image path or URL
+        width: 1200,
+        height: 630,
+        alt: "An Vu Vuong Portfolio Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
